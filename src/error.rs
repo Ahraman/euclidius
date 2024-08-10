41d266f56ba835fa46d59471c086aba0adf2be6f
Ahraman <ahraman12000@gmail.com>
2024-08-10 03:53:41 +0330

    Added axum v0.7.5 as dependency.

--- src/error.rs
+++ src/error.rs
@@ -1,17 +1,25 @@
 #[derive(Debug)]
 pub enum Error {
+    Io(std::io::Error),
     Dotenvy(dotenvy::Error),
 }
 
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
+            Error::Io(e) => e.fmt(f),
             Error::Dotenvy(e) => e.fmt(f),
         }
     }
 }
 
 impl std::error::Error for Error {}
+
+impl From<std::io::Error> for Error {
+    fn from(value: std::io::Error) -> Self {
+        Self::Io(value)
+    }
+}
 
 impl From<dotenvy::Error> for Error {
     fn from(value: dotenvy::Error) -> Self {
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
+use axum::{
+    extract::Path,
+    response::{IntoResponse, Redirect},
+    routing::get,
+};
 use euclidius::error::Error;
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     dotenvy::dotenv()?;
-    println!("Hello, world!");
+
+    let listener = TcpListener::bind("localhost:3000").await?;
+    axum::serve(listener, build_router()).await?;
 
     Ok(())
+}
+
+pub fn build_router() -> axum::Router {
+    axum::Router::new()
+        .route("/", get(root))
+        .route("/:page", get(show_page))
+}
+
+async fn root() -> impl IntoResponse {
+    Redirect::to("/main")
+}
+
+async fn show_page(Path(page): Path<String>) -> impl IntoResponse {
+    format!("Looking for page '{page}'.")
 }
